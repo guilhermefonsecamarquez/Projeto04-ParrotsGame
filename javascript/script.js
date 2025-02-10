8791eab97b120d5a037f2a3938539c81fa419542
Guilherme Fonseca <guilhermefonsecamarquez@gmail.com>
2025-02-10 11:35:31 -0300

    checagem-embaralhamento-distribuição-de-cartas-javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,56 @@
-let numCards = prompt("quantas cartas quer jogar?");
+let numCards = prompt("Quantas cartas quer jogar?");
+const allCardsNames = ['bobrossparrot', 'bobrossparrot',
+                      'explodyparrot', 'explodyparrot',
+                      'fiestaparrot', 'fiestaparrot',
+                      'metalparrot', 'metalparrot',
+                      'revertitparrot', 'revertitparrot',
+                      'tripletsparrot', 'tripletsparrot',
+                      'unicornparrot', 'unicornparrot'];
+let selectedCards = [];
+let cards = document.querySelector('.container');
 
 function checkNumCards() {
   if (numCards%2 === 0 && numCards <= 14 && numCards > 2) {
-    alert("entrando no jogo");
+    fillSelectedCards();
   } else {
-    numCards = prompt("Número invalido! quantas cartas quer jogar?");
+    numCards = prompt("Número inválido! Quantas cartas quer jogar?");
     checkNumCards();
   }
 }
 
-checkNumCards();
+function fillSelectedCards() {
+  for (let index = 0; index < numCards; index++) {
+    selectedCards[index] = allCardsNames[index];    
+  }
+  shuffleCards();
+}
+
+function comparator() { 
+  return Math.random() - 0.5; 
+}
+
+function shuffleCards() {
+  selectedCards.sort(comparator);
+}
+
+function putCards() {
+  for (let index = 0; index < numCards; index++) {
+    cards.innerHTML += `
+    <div class="card" onclick="flipCard(this)">
+      <div class="img-card front">
+        <img src="assets/back.png" alt="">
+      </div>
+      <div class="img-card back">
+        <img src="assets/${selectedCards[index]}.gif" alt="">
+      </div>
+    </div>`;
+  }
+
+}
 
 function flipCard(card) {
   card.classList.toggle('virada');
 }
+
+checkNumCards();
+putCards();
