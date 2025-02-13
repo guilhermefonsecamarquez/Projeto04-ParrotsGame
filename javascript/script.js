85b7c5669fdd6594e25cb3eed14ed5235ee2ae08
Guilherme Fonseca <guilhermefonsecamarquez@gmail.com>
2025-02-13 10:02:27 -0300

    funçoes-de-comparar-cartas-para-a-jogabilidade-ideal

--- javascript/script.js
+++ javascript/script.js
@@ -8,6 +8,9 @@ const allCardsNames = ['bobrossparrot', 'bobrossparrot',
                       'unicornparrot', 'unicornparrot'];
 let selectedCards = [];
 let cards = document.querySelector('.container');
+let checkFlipCards = [];
+let countFlipCards = 0;
+let countNumbersCards = 0;
 
 function checkNumCards() {
   if (numCards%2 === 0 && numCards <= 14 && numCards > 2) {
@@ -35,21 +38,59 @@ function shuffleCards() {
 
 function putCards() {
   for (let index = 0; index < numCards; index++) {
+    countNumbersCards++;
     cards.innerHTML += `
-    <div class="card" onclick="flipCard(this)">
-      <div class="img-card front">
-        <img src="assets/back.png" alt="">
-      </div>
-      <div class="img-card back">
-        <img src="assets/${selectedCards[index]}.gif" alt="">
-      </div>
-    </div>`;
+                      <div id="card${countNumbersCards}">
+                        <div class="card ${selectedCards[index]}" onclick="flipCard(this)">
+                          <div class="img-card front">
+                            <img src="assets/back.png" alt="">
+                          </div>
+                          <div class="img-card back">
+                            <img src="assets/${selectedCards[index]}.gif" alt="">
+                          </div>
+                        </div>
+                      </div>`;
   }
 
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 function flipCard(card) {
-  card.classList.toggle('virada');
+  card.classList.add('virada');
+  checkFlipCards[countFlipCards] = card.parentNode.id;
+  countFlipCards++;
+  checkFlippedNumbersCards();
+}
+
+function checkFlippedNumbersCards() {
+  if (countFlipCards === 2) {
+    if (document.querySelector(`#${checkFlipCards[0]} .virada`).classList[1] !== document.querySelector(`#${checkFlipCards[1]} .virada`).classList[1]) {
+      setTimeout(untapCards, 1000, checkFlipCards[0], checkFlipCards[1]);
+    } else {
+      for (let index = 0; index < countFlipCards; index++) {
+        const nameCard = document.querySelector(`#${checkFlipCards[index]} .virada`).classList[1];
+
+        document.querySelector(`#${checkFlipCards[index]}`).innerHTML = `
+          <div class="card ${nameCard} virada">
+            <div class="img-card front">
+              <img src="assets/back.png" alt="">
+            </div>
+            <div class="img-card back">
+              <img src="assets/${nameCard}.gif" alt="">
+            </div>
+          </div>`;
+
+           
+      }
+    }
+    countFlipCards = 0;
+  }
+}
+
+function untapCards(card1, card2) {
+  document.querySelector(`#${card1} .virada`).classList.remove('virada');
+  document.querySelector(`#${card2} .virada`).classList.remove('virada');
 }
 
 checkNumCards();
